1e6026e0ebe70d96f436482592c1c60ec2107635
Deepankar Gupta <deepankargupta874@gmail.com>
2025-10-25 18:33:35 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a chrome plugin to analyze youtube comments of a video',
+    author='deepankar',
+    license='',
+)
