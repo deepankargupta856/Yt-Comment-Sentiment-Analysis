b959ae39ae4de0310e01f9fb623568de9bfcc176
Deepankar Gupta <deepankargupta874@gmail.com>
2025-10-26 18:44:47 +0530

    added deployment and containerization

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -127,7 +127,7 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
 
 
 def main():
-    mlflow.set_tracking_uri("http://ec2-13-53-74-103.eu-north-1.compute.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://ec2-13-48-193-8.eu-north-1.compute.amazonaws.com:5000/")
 
     mlflow.set_experiment('dvc-pipeline-runs')
     
